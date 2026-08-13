7f9f409a2d0be98d5c2964efa10a233875b7a847
FontVicius <prog.sevenrockradio@gmail.com>
2026-08-13 11:45:43 +0200

    Instalar Google Tag Manager en layouts principales

--- resources/views/components/layouts/site.blade.php
+++ resources/views/components/layouts/site.blade.php
@@ -144,6 +144,13 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" style="scrollbar-gutter: stable;">
 <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5NCG6PN');</script>
+    <!-- End Google Tag Manager -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -219,6 +226,10 @@
     @stack('styles')
 </head>
 <body class="antialiased">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5NCG6PN"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <div class="lucille-fixed-bg" aria-hidden="true"></div>
 
     <x-navigation.rocks-menu :theme="$theme" />
