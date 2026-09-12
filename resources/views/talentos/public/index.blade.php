58ca0e34e6ef118217082f7fa8819171efcd8f51
FontVicius <prog.sevenrockradio@gmail.com>
2026-09-12 19:33:42 +0200

    Use uploaded rock wall image for banner background

--- resources/views/talentos/public/index.blade.php
+++ resources/views/talentos/public/index.blade.php
@@ -11,7 +11,7 @@
         <!-- Section Header Banner (Impeccable Design) -->
         <div class="relative overflow-hidden mb-8 rounded-[20px] border border-white/10 p-8 md:p-12 shadow-[0_20px_50px_rgba(0,0,0,0.6)] group">
             {{-- Background Image --}}
-            <div class="absolute inset-0 z-0 bg-cover bg-center transition-transform duration-700 group-hover:scale-105" style="background-image: url('{{ asset('assets/lucille/muro-bg.png') }}');"></div>
+            <div class="absolute inset-0 z-0 bg-cover bg-center transition-transform duration-700 group-hover:scale-105" style="background-image: url('{{ asset('assets/lucille/rock-wall.jpg') }}');"></div>
             
             {{-- Glass Overlay (Vidrio) - Less opacity to make image visible --}}
             <div class="absolute inset-0 z-0 bg-gradient-to-br from-[#161214]/40 via-[#0f0e11]/50 to-[#08080a]/75 backdrop-blur-[3px] pointer-events-none"></div>
