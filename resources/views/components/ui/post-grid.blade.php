b44685b2edc706f8c353d327d90580569160db9d
FontVicius <prog.sevenrockradio@gmail.com>
2026-09-23 10:39:22 +0200

    Fix button alignment in news cards

--- resources/views/components/ui/post-grid.blade.php
+++ resources/views/components/ui/post-grid.blade.php
@@ -23,11 +23,11 @@
             @else
                 <div class="absolute inset-0 bg-[rgba(7,16,33,.45)]"></div>
             @endif
-            <div class="relative z-10 flex min-h-[455px] flex-col items-center justify-center px-11 py-10">
+            <div class="relative z-10 flex min-h-[455px] flex-col items-center px-11 py-10">
                 <h3 class="font-display text-[26px] font-normal uppercase leading-tight tracking-[1px] text-[#f9f9f9] transition duration-300 group-hover:text-lucille-accent">{{ $title }}</h3>
                 <p class="mt-3 text-sm italic leading-[26px] text-[#cbcbcb]">{{ $date }} by admin in {{ $category }}</p>
                 <p class="mt-7 text-[14px] leading-[26px] text-[#d8d8d8]">{{ $excerpt }}</p>
-                <a href="{{ $url }}" class="lucille-button mt-6 min-h-[36px] px-4 text-[11px]">Read More</a>
+                <a href="{{ $url }}" class="lucille-button mt-auto min-h-[36px] px-4 text-[11px]">Read More</a>
             </div>
         </article>
     @endforeach
